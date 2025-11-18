b687e86cd4e99bb490b168b862026e21a6d696b6
GitHub Action <action@github.com>
2025-11-18 16:00:57 +0000

    Clean up template

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "kotlinx-benchmark-kmp-template"
+rootProject.name = "to-list-vs-to-array-transformations"
 
